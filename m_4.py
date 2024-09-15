4798875d06392287d28d3c71e75849c1b404e48d
KikaKar <drxaos1@gmail.com>
2024-09-15 17:04:28 +0300

    Task 1 v1.1

--- m_4.py
+++ m_4.py
@@ -1,9 +1,16 @@
 #Завдання № 1
-path = open('salary_file.txt', mode='r')
+path = 'salary_file.txt'
 def total_salary(path):
-    
-    symbols = path.read()
-    print(symbols)
-    path.close()
+    total = 0 # Загальна сума зарплат
+    workers = 0 # Кількість робітників
+    with open(path, 'r') as file: # Відкриття файлу 
+        for line in file: 
+            name, salary = line.strip().split(',') # Розділення рядка на ім'я робітників та зарплату
+            salary = int(salary)  # Перетворюємо зарплату на число
+            total += salary  # Обрахунок заробітньої плати загалом
+            workers += 1 # Підрахунок робітників 
+        average = total / workers if workers > 0 else 0 # Обрахунок середгьої заробітньої платні 
+        return total, average
 
-total_salary(path)
+total, average = total_salary(path)
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
