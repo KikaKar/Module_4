c0fb5e79040448f6a69cf39ceddddaea93aa3843
KikaKar <drxaos1@gmail.com>
2024-09-23 23:22:53 +0300

    Task 2 v0.1

--- m_4_t_2.py
+++ m_4_t_2.py
@@ -2,4 +2,10 @@
 path = 'cats_file.txt' # Змфна шляху до файлу
 def get_cats_info(path):
     with open(path, 'r') as file: # Відкриття файлу
-        
+        cats_data = []
+        for line in file:
+            id, name, age = line.strip().split(',') # Розділення рядка на id номер, ім'я кота, рік 
+            age = int(age) # Перетворення років на ціле число
+            cats_data.append(dict(id = id, name = name, age = age))
+            print(cats_data)
+get_cats_info(path)
