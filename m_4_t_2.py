d88e31895e81e68a294a9b05cc111b943dae7eea
KikaKar <drxaos1@gmail.com>
2024-09-25 22:20:06 +0300

    Task 2 v0.2

--- m_4_t_2.py
+++ m_4_t_2.py
@@ -1,11 +1,19 @@
 # Завдання № 2
 path = 'cats_file.txt' # Змфна шляху до файлу
 def get_cats_info(path):
-    with open(path, 'r') as file: # Відкриття файлу
-        cats_data = []
-        for line in file:
-            id, name, age = line.strip().split(',') # Розділення рядка на id номер, ім'я кота, рік 
-            age = int(age) # Перетворення років на ціле число
-            cats_data.append(dict(id = id, name = name, age = age))
-            print(cats_data)
-get_cats_info(path)
+    try:
+        with open(path, 'r') as file: # Відкриття файлу
+            cats_data = [] # Створення списку для словників
+            for line in file:
+                
+                id, name, age = line.strip().split(',') # Розділення рядка на id номер, ім'я кота, рік 
+                age = int(age) # Перетворення років на ціле число
+                cats_dict = dict(id = id, name = name, age = age) # Створення словника із ключами: id, name, age та відповідними значеннями
+                cats_data.append(cats_dict) # Додавання словників у список
+            return cats_data
+    except FileNotFoundError: # Перевірка на відсутність файлу
+        print(f'Файл {path} не знайдено.')
+        return 0, 0 
+    except IOError:
+        print(f'Помилка при відкриті файлу {path}.')
+print(get_cats_info(path))
